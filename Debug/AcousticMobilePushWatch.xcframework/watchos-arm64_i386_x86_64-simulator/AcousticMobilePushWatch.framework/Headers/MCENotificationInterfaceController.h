393338edf32d17b015999e0cd56b78b037ef4703
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2023-12-12 20:44:40 -0800

    Beta AcousticMobilePush build: 3.8.8
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements:
    
    - Update for xcode 15.x.
    - Add to cocoapods public site.
    - Add Carthage support.
    
    Our environment for this release:
    
    - Xcode 15.0.1
    - MacOS 14.1.2
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    	- watch
    		- arm64
    		- arm64_32
    		- armv7k
    		- i386
    		- x86_64
    	- macos
    		- arm64
    		- x86_64-maccatalyst
    		- x86_64-simulator
    
    Known Issues
    
    - ?

---
+++ Debug/AcousticMobilePushWatch.xcframework/watchos-arm64_i386_x86_64-simulator/AcousticMobilePushWatch.framework/Headers/MCENotificationInterfaceController.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2017, 2019 Acoustic, L.P. All rights reserved.
+ *
+ * NOTICE: This file contains material that is confidential and proprietary to
+ * Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+ * industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+ * Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+ * prohibited.
+ */
+
+
+#if __has_feature(modules)
+@import WatchKit;
+#else
+#import <WatchKit/WatchKit.h>
+#endif
+
+@interface MCENotificationInterfaceController : WKUserNotificationInterfaceController
+
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *headerImage;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *titleLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceMap *mapView;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *backgroundGroup;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *bodyLabel;
+
+@end
---
+++ Debug/AcousticMobilePushWatch.xcframework/watchos-arm64_i386_x86_64-simulator/AcousticMobilePushWatch.framework/Headers/MCEWatchActionRegistry.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2017, 2019 Acoustic, L.P. All rights reserved.
+ *
+ * NOTICE: This file contains material that is confidential and proprietary to
+ * Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+ * industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+ * Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+ * prohibited.
+ */
+
+
+#if __has_feature(modules)
+@import Foundation;
+#else
+#import <Foundation/Foundation.h>
+#endif
+
+@interface MCEWatchActionRegistry : NSObject
+
+/** This method returns the singleton object of this class. */
+@property(class, nonatomic, readonly) MCEWatchActionRegistry * _Nonnull sharedInstance NS_SWIFT_NAME(shared);
+
+/** This method is used to register an object to receive action messages for a specified action type name.
+ 
+ @param target the object that will accept action messages
+ @param selector a selector that processes the action, can either take one or two arguments. The first argument is always the action payload and the second, if included is the full APNS payload.
+ @param type action the specified action type name to be used in the APNS payload as the type value
+ 
+ @return TRUE or FALSE depending if the registration was successful or not.
+ 
+ */
+- (BOOL) registerTarget: (id _Nonnull) target withSelector: (SEL _Nonnull) selector forAction:( NSString * _Nonnull) type;
+
+/** This method performs the registered specified action for the APNS payload.
+ 
+ @param action the action dictionary to be executed. (either the "notification-action" or one of the "category-actions")
+ @param payload the full APNS payload
+ @param source the event type value to report
+ @param attributes Additional attributes for event payload
+ 
+ */
+
+-(void)performAction:(NSDictionary * _Nonnull)action forPayload:(NSDictionary * _Nonnull)payload source: (NSString * _Nonnull) source attributes: (NSDictionary * _Nullable) attributes;
+
+@end
+
